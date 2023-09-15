bde9faa3f3ec00edfb282eeb4810ec25c31be876
dahee-lee <dahlee97@gmail.com>
2023-09-14 22:31:10 -0400

    Show, Edit and Update Routes Created

--- controllers/pens.js
+++ controllers/pens.js
@@ -1,27 +1,54 @@
-const express=require("express")
-const router=express.Router()
-const Pen=require("../models/pens")
+const express = require("express")
+const router = express.Router()
+const Pen = require("../models/pens")
 
 // Index Route
 router.get('/', async (request, response) => {
     const foundEntries = await Pen.find()
-    response.render("index.ejs",{
+    response.render("index.ejs", {
         pens: foundEntries
     })
- })
+})
 
 // New Route
-router.get("/new", (request, response)=>{
+router.get("/new", (request, response) => {
     response.render("new.ejs")
 })
 
 // Create Route
-router.post("/", async (request,response)=>{
-    try{const newEntry = await Pen.create(request.body)
-    response.redirect("/pens")
-    }catch(err){
+router.post("/", async (request, response) => {
+    try {
+        const newEntry = await Pen.create(request.body)
+        response.redirect("/pens")
+    } catch (err) {
         console.log(err)
     }
 })
 
-module.exports=router
+// Show Route
+router.get("/:id", async (request,response) => {
+    const oneEntry = await Pen.findById(request.params.id)
+    response.render("show.ejs",{
+        pens: oneEntry
+    })
+})
+
+// Edit Route
+router.get("/:id/edit", async (request, response) => {
+    const foundEntry = await Pen.findById(request.params.id)
+    response.render("edit.ejs", {
+        pens: foundEntry
+    })
+})
+
+// Update Route
+router.put("/:id", async (request,response) => {
+    const updatedEntry = await Pen.findByIdAndUpdate(request.params.id, request.body, {new:true})
+    console.log(updatedEntry)
+    response.redirect(`/pens/${request.params.id}`)
+})
+
+// Delete Route
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 // IMPORTS
 const express = require('express')
 const app = express()
-const Pen=require("./models/pens")
+const Pen = require("./models/pens")
+const methodOverride = require("method-override")
 
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
 
 const mongoose = require('mongoose')
 const mongoURI = process.env.MONGO_URI
@@ -14,15 +20,12 @@ const mongoURI = process.env.MONGO_URI
 mongoose.connect(mongoURI)
 const db = mongoose.connection
 // optional create status messages to check mongo connection 
-db.on('error', (err) => { console.log('ERROR: ' , err)})
-db.on('connected', () => { console.log('mongo connected')})
-db.on('disconnected', () => { console.log('mongo disconnected')})
-
-// MIDDLEWARE
-app.use(express.urlencoded({extended:true}))
+db.on('error', (err) => { console.log('ERROR: ', err) })
+db.on('connected', () => { console.log('mongo connected') })
+db.on('disconnected', () => { console.log('mongo disconnected') })
 
 
-const pensController=require("./controllers/pens")
+const pensController = require("./controllers/pens")
 app.use("/pens", pensController)
 
 
