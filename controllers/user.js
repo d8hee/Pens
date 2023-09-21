229403ffcbf992c03d3275967cbd946a45bec35e
dahee-lee <dahlee97@gmail.com>
2023-09-20 22:49:51 -0400

    New User Registration form successfully POSTs to DB

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,31 @@ const express = require("express")
 const router = express.Router()
 const bcrypt = require("bcrypt")
 const Pen = require("../models/pens")
-const User = require("../models/pensuser.js")
+const User = require("../models/pensuser")
 
+//New User Sign Up Form
 router.get("/", (request,response)=>{
     response.render("newUser.ejs")
 })
+
+// New User Sign Up Form POST
+router.post("/", async (request,response) => {
+    try{
+        request.body.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10))
+    const newUser = await User.create(request.body)
+    request.session.currentUser = newUser
+    console.log(newUser)
+    response.redirect("/pens")
+    }
+    catch (err){
+        console.log(err)
+        response.status(500).send("error, please try again")
+    }
+})
+
+// Login User Route
+
+// Logout User Route
 
 // // User Profile Page Route
 // router.get("/profile", (request,response)=>{
@@ -14,16 +34,6 @@ router.get("/", (request,response)=>{
 // })
 
 
-// // New User Sign Up Form
-// router.get("/register", (request,response)=>{
-//     response.render("newUser.ejs")
-// })
-// // New User Sign Up Form POST
-// router.post("/", async (request,response) => {
-//     request.body.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10))
-//     const newUser = await User.create(request.body)
-//     request.session.currentUser = newUser
-//     response.redirect("/user")
-// })  
+
 
 module.exports = router
